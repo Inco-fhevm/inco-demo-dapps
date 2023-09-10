f2e5907d0793f30c1db48afde79643fad5dece9f
remi <remigai@bu.edu>
2023-09-10 13:09:30 -0400

    setup layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
+import "./App.css";
 import { useState, useEffect } from "react";
-import { Form, Button } from "react-bootstrap";
 import { init, getInstance } from "./utils/fhevm";
-import { toHexString } from "./utils/utils";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { Connect } from "./Connect";
+import ConfidentialERC20 from "./components/erc20/ConfidentialERC20"; // Import the component
 
 function App() {
   const [isInitialized, setIsInitialized] = useState(false);
@@ -19,21 +19,49 @@ function App() {
   if (!isInitialized) return null;
 
   return (
-    <div className='App'>
-      <div className="menu">
-        <Connect>{(account, provider) => <Example />}</Connect>
+    <Router>
+      <div className="App flex flex-col justify-center h-screen font-press-start text-black">
+        <div className="menu">
+          <Connect>
+            {(account, provider) => (
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route
+                  exact
+                  path="/confidential-erc20"
+                  element={<ConfidentialERC20 />}
+                />
+                {/* Add more routes for other pages here */}
+              </Routes>
+            )}
+          </Connect>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">dApps List:</h1>
+      <div className="flex flex-col">
+        <Link to="/confidential-erc20">Confidential ERC-20</Link>
+        <Link to="/hidden-card">Hidden Card</Link>
+        <Link to="/private-voting">Private Voting</Link>
+        <Link to="/smart-wallet-otp">Smart Wallet OTP</Link>
+        <Link to="/confidential-did">Confidential DID</Link>
+      </div>
+      <br></br>
+      <h1 className="text-3xl font-bold underline">Tools:</h1>
+      <div className="flex flex-col">
+        <Link to="/faucet">Faucet</Link>
+        <Link to="/ciphertext-generator">Ciphertext Generator</Link>
+        <Link to="/sample-codes">Sample Codes</Link>
+        <Link to="/documentation">Documentation</Link>
       </div>
     </div>
-  )
+  );
 }
 
-function Example() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/erc20/ConfidentialERC20.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ConfidentialERC20() {
+  return (
+    <div>
+      <h1>Hello, World! This is the Confidential ERC-20 page.</h1>
+      <Link to="/">Back to Main Page</Link>
+      {/* Add your content here */}
+    </div>
+  );
+}
+
+export default ConfidentialERC20;
